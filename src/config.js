d20924c9c49f2988e1258886a90624684fa0ee24
Anthony Snow <>
2019-10-16 16:43:49 -0400

    test

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-exports.ACCESS_TOKEN = "vJuUKghwZaNsBh0vqqo95xMuQQAERAmUaQ97CWOh";
-exports.BOT_ID = "207c028467b31d45108bb15e69";
-exports.GROUP_ID = "54917230";
-exports.GIHPY_KEY = '6ZO6RfzYXgXTaI9XMPHQMigvbIsj0u0x';
+exports.ACCESS_TOKEN = "vJuUKghwZaNsBh0vqqo95xMuQQAERAmUaQ97CWOh"; //EDIT
+exports.BOT_ID = "534d007d0e8305a6cdbef3ee54"; //EDIT
+exports.GROUP_ID = "54942304"; //EDIT
+exports.GIHPY_KEY = '6ZO6RfzYXgXTaI9XMPHQMigvbIsj0u0x'; //EDIT
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 
