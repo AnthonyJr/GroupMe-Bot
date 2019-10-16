53f63bd5ea4f1e39159d00c4ad5f1ec2bd97dfdd
Anthony Terrell Snow Jr <anthony.snow@twinspires.com>
2019-10-15 21:08:57 -0400

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-exports.ACCESS_TOKEN = "GroupMe access token";
-exports.BOT_ID = "GroupMe bot ID";
-exports.GROUP_ID = "GroupMe group id";
-exports.GIHPY_KEY = 'Giphy API key';
+exports.ACCESS_TOKEN = "vJuUKghwZaNsBh0vqqo95xMuQQAERAmUaQ97CWOh";
+exports.BOT_ID = "207c028467b31d45108bb15e69";
+exports.GROUP_ID = "54917230";
+exports.GIHPY_KEY = '6ZO6RfzYXgXTaI9XMPHQMigvbIsj0u0x';
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
 
